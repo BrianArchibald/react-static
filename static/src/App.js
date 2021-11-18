73ad1bedef20049c7da647939c16c82e8e09e0c9
Brian Archibald <brianallenarchibald@gmail.com>
2021-11-18 15:27:29 -0600

    static site

--- static/src/App.js
+++ static/src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './styles.css';
+import Navbar from './components/Navbar'
+import Main from './components/Main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar />
+      <Main />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Fun Facts about React</h1>
         <ul>
-          <li>First released in 2016</li>
+          <li>First released in 2013</li>
           <li>Created by Jordan Walke</li>
           <li>Over 100k stars on GitHub</li>
           <li>Maintained by Facebook</li>
           <li>Powers many enterprise apps</li>
         </ul>
-      </header>
+      </header> */}
     </div>
   );
 }
+
 
 export default App;
